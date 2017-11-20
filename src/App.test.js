ab24017af8a25fd5050286baecc6d4e276bc3877
wemmm <jenny.wem@gmail.com>
2017-11-20 14:14:00 +0000

    Adds todoList.js, renders on index.html

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TodoList from './TodoList';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders the TodoList component', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<TodoList />, div);
 });
